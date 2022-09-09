ef31820bb06720d2f3f06537611baf949fc38cbb
Sho Tsukamoto <sho.tsuka.0709@gmail.com>
2022-09-09 18:31:11 +0900

    Added Request Date View.

---
+++ app/UseCases/Date/GetRequestDateAction.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\UseCases\Date;
+
+use Carbon\Carbon;
+
+class GetRequestDateAction
+{
+    public function __invoke($request): Carbon
+    {
+        return new Carbon($request['year'] . '-' . sprintf('%02d',$request['month']) . '-' . sprintf('%02d',$request['date']));
+    }
+}
--- index.php
+++ index.php
@@ -2,11 +2,16 @@
 
 require_once __DIR__ . "/vendor/autoload.php";
 
+use App\UseCases\Date\GetRequestDateAction;
+use Carbon\Carbon;
 use App\Services\TideGraphService;
 use App\UseCases\Date\ValidateRequestDateAction;
 
+Carbon::setLocale('ja');
+
 try {
     $request = (new ValidateRequestDateAction())($_REQUEST);
+    $date = (new GetRequestDateAction())($_REQUEST);
     $tide_data = (new TideGraphService($request['year'], $request['month'], $request['date']));
     $tide_data_array = $tide_data->get_tide_data_array();
 
@@ -27,11 +32,22 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name=”viewport” content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no">
+    <!--    <meta name="viewport" content="width=device-width, initial-scale=1.0">-->
     <script src="//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.js"></script>
     <style>
+        body {
+            -webkit-text-size-adjust: 100%;
+        }
+
         .content-wrap {
             position: relative;
+        }
+
+        .chart-container {
+            position: relative;
+            width: 100vw;
+            height: 100vh;
         }
 
         .tide-description {
@@ -42,8 +58,9 @@ try {
             opacity: 0.7;
             padding: 1rem;
             border: 1px solid #eee;
-            max-width: 200px;
+            max-width: 250px;
             width: 100%;
+            z-index: 2;
         }
 
         .dl {
@@ -60,12 +77,31 @@ try {
             width: 70%;
             margin: 0;
         }
+
+        .date {
+            font-size: 1.2rem;
+            font-weight: bold;
+            text-align: center;
+            margin-top: 0;
+            margin-bottom: 1.5rem;
+        }
+
+        @media(max-width: 767px) {
+            .chart-container {
+                position: relative;
+                width: 100vw;
+                height: 60vh;
+            }
+        }
     </style>
 </head>
 
 <body>
 <div class="content-wrap">
     <div class="tide-description">
+        <h1 class="date">
+            <?php echo $date->isoFormat('YYYY年MM月DD日(ddd)'); ?>
+        </h1>
         <dl class="dl">
             <dt class="dt">
                 日の出
@@ -103,13 +139,15 @@ try {
             </dd>
         </dl>
     </div>
-    <canvas class="canvas" id="chart"></canvas>
+    <div class="chart-container">
+        <canvas class="canvas" id="chart"></canvas>
+    </div>
 </div>
 <script>
     const tide_data = <?php echo json_encode($tide_data_array['tide'], JSON_THROW_ON_ERROR); ?>;
     const ctx = document.getElementById("chart");
 
-    const get_dataset = function (parent_data,type) {
+    const get_dataset = function (parent_data, type) {
         let array = [];
         for (let i = 0; i < parent_data.length; i++) {
             array.push(parent_data[i][type])
@@ -128,12 +166,20 @@ try {
             "datasets": [{
                 "label": "潮位",
                 "data": tide_cm,
-                "fill": false,
-                "borderColor": "rgb(75, 192, 192)",
-                "lineTension": 0.1
+                "fill": true,
+                "borderColor": "#35b0eb",
+                "backgroundColor":"#a9e3ff",
+                "lineTension": 0.5,
+                "borderWidth": 7,
+                "pointHoverBorderWidth": 10,
             }]
         },
-        options: {},
+        options: {
+            title: {},
+            legend: {},
+            scales: {},
+            maintainAspectRatio: false //これを追加
+        },
     });
 </script>
 </body>
